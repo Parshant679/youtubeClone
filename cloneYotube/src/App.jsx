c9cdf5d77c6d426dea16ec2fe86ed5a580c57ed8
Parshant Rajput <54788475+Parshant679@users.noreply.github.com>
2024-02-29 15:07:31 +0530

    stage 2

--- cloneYotube/src/App.jsx
+++ cloneYotube/src/App.jsx
@@ -1,12 +1,19 @@
-import { useState } from 'react'
-import './App.css'
-import Home from './components/Home'
+import { useState } from "react";
+import "./App.css";
+import Home from "./pages/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Search from "./pages/Search";
+import Watch from "./pages/Watch";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <Home></Home>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Watch/:id" element={<Watch />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
